4cfa810c354d70e3e7ebd1ec5de9540a41b4df1e
justin <JustinCarpenter2020@Gmail.com>
2023-06-17 10:46:43 -0600

    test contact on azure

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,12 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'smtp.elasticemail.com',
+    'username' => 'JacobCarpenter407@Gmail.com',
+    'password' => 'B1E2FB35571DF88A0F10619A49FBBA9F28A0',
+    'port' => '2525'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
